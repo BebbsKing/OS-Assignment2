3e5cfe89c6afde9f937526d09945bac955062138
Brodie <brodie.ebbs@gmail.com>
2021-09-25 11:27:13 +0930

    Updated compare script and the readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # OS-Assignment2
+
+
+To compile schedulers and run comparison, run compare.sh in root directory with the input data you want to test e.g.
+./compare.sh data_1111.txt
+or
+./compare.sh inputs/data_1111.txt
+(either should work)
--- compare.sh
+++ compare.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
+if [[ $# -ne 1 ]]; then
+    test="data_1111.txt"
+else
+    test=$1
+fi
+
+if [[ $test != "inputs/"* ]]; then
+    test=$(echo "inputs/${test}")
+fi
+
+baseline=$(echo "outputs/baseline_$(echo $test | cut -d "_" -f 2)")
+scheduler=$(echo "outputs/scheduler_$(echo $test | cut -d "_" -f 2)")
+
 g++ baseline.cpp -o bin/baseline
 g++ scheduler.cpp -o bin/scheduler
 g++ compare_stats.cpp -o bin/compare_stats
 
-./bin/baseline inputs_outputs/data_1111.txt inputs_outputs/baseline_1111.txt
-./bin/scheduler inputs_outputs/data_1111.txt inputs_outputs/scheduler_1111.txt
+./bin/baseline $test $baseline
+./bin/scheduler $test $scheduler
 
-./bin/compare_stats inputs_outputs/data_1111.txt inputs_outputs/baseline_1111.txt inputs_outputs/scheduler_1111.txt
+./bin/compare_stats $test $baseline $scheduler
